3c0f22b6d232bdb2e1392cb63e9db501c6bf7930
Kyungyun_host <kyungyun98@gmail.com>
2022-04-03 21:09:13 +0900

    input process

--- project1/main.c
+++ project1/main.c
@@ -1,10 +1,24 @@
 #include "mylib.h"
 
-int current_mode = 1;
-
 int main(int argc, char **argv)
 {
     pid_t p1 = 0, p2 = 0;
+    int shm_input_id, shm_output_id;
+    
+    // Create shared memory
+    shm_input_id = shmget((key_t)SHARED_MEM_KEY_IN, sizeof(input_shm), IPC_CREAT | 0666);
+    if (shm_input_id < 0)
+    {
+        printf("input shmget error\n");
+        exit(-1);
+    }
+
+    shm_output_id = shmget((key_t)SHARED_MEM_KEY_OUT, sizeof(output_shm), IPC_CREAT | 0666);
+    if (shm_output_id < 0)
+    {
+        printf("output shmget error\n");
+        exit(-1);
+    }
 
     p1 = fork();
 
@@ -13,14 +27,16 @@ int main(int argc, char **argv)
     {
         p2 = fork();
 
-        if (p2 > 0)
-        {
-            // main process
-        }
-
-        else if (p2 == 0)
+        if (p2 == 0)
         {
             // input process
+            input_process(shm_input_id);
+        }
+
+        else if (p2 > 0)
+        {
+            // main process
+            main_process(shm_input_id, shm_output_id);
         }
 
         else
@@ -34,6 +50,7 @@ int main(int argc, char **argv)
     else if (p1 == 0)
     {
         // output process
+        output_process(shm_output_id);
     }
 
     else
@@ -41,6 +58,14 @@ int main(int argc, char **argv)
         printf("Fork Failed\n");
         exit(-1);
     }
+
+    // Wait for child process
+    wait(p1);
+    wait(p2);
+
+    // Erase shared memory
+    shmctl(shm_input_id, IPC_RMID, (struct shmid_ds *)NULL);
+    shmctl(shm_output_id, IPC_RMID, (struct shmid_ds *)NULL);
 
     return 0;
 }
--- project1/process/input_process.c
+++ project1/process/input_process.c
@@ -1,7 +1,121 @@
+/*****************************************
+ *
+ * Input process :
+ *
+ * 1. Get input from device key and switch
+ * 2. Push inputs to main process
+ *    by using shared memory and semaphore
+ *
+ *****************************************/
+
 #include "mylib.h"
 
-extern int current_mode;
+#define KEY_RELEASE 0
+#define KEY_PRESS 1
 
-void input_process() {
+void input_process(int shm_id)
+{
+    int dev_key, dev_sw, sem_id;
+    input_shm *shm_addr;
+    int exit = FALSE;
 
+    // Open key device file
+    char *key_file = "/dev/input/event0";
+    if ((dev_key = open(dev_key, O_RDONLY)) < 0)
+    {
+        printf("%s is not a vaild device\n", key_file);
+        return ;
+    }
+
+    // Open switch device file
+    char *sw_file = "/dev/fpga_push_switch";
+    if ((dev_sw = open(dev_sw, O_RDONLY)) < 0)
+    {
+        printf("%s is not a vaild device\n", sw_file);
+        return ;
+    }
+
+    // Attach shared memory
+    shm_addr = (input_shm *)shmat(shm_id, (void *)0, 0);
+    if (shm_id < 0)
+    {
+        printf("shmat error\n");
+        return ;
+    }
+
+    // Create Semaphore
+    sem_id = seminit();
+
+    printf("Input process is successfully started\n");
+    printf("Input process is Hearing...\n");
+
+    while (exit == FALSE)
+    {
+        // exit is chagned in main process
+        if (shm_addr->exit == TRUE)
+            exit = shm_addr->exit;
+        // Key input
+        key_action(dev_key, shm_addr, sem_id);
+        // Switch input
+        switch_action(dev_sw, shm_addr, sem_id);
+    }
+
+    // Detach shahred memory
+    shmdt((char *)shm_addr);
+
+    // Close device files
+    close(dev_key);
+    close(dev_sw);
+
+    return ;
+}
+
+int key_action(int dev_key, input_shm *shm_addr, int sem_id)
+{
+    struct input_event ev[BUFF_SIZE];
+    int key_size = sizeof(struct input_event);
+
+    if (read(dev_key, ev, key_size * BUFF_SIZE) < 0)
+    {
+        printf("Key input error\n");
+        return -1;
+    }
+
+    // Accessing Critical Section
+    semalock(sem_id);
+    shm_addr->key_code = ev[0].code;
+    semaunlock(sem_id);
+
+    return 0;
+}
+
+void switch_action(int dev_sw, input_shm *shm_addr, int sem_id)
+{
+    unsigned char push_sw_buff[MAX_BUTTON];
+    unsigned char result[MAX_BUTTON];
+    int sw_size = sizeof(push_sw_buff);
+
+    memset(result, 0, sw_size);
+
+    // Processing when simultaneous input
+    for (int i = 0; i < 10000; i++)
+    {
+        if (read(dev_sw, &push_sw_buff, sw_size) < 0)
+        {
+            printf("Switch input error\n");
+            return -1;
+        }
+
+        for (int j = 0; j < MAX_BUTTON; j++)
+        {
+            result[j] = result[j] | push_sw_buff[j];
+        }
+    }
+
+    // Accessing ritical Section
+    semalock(sem_id);
+    memcpy(shm_addr->sw, result, sw_size);
+    semaunlock(sem_id);
+
+    return 0;
 }
