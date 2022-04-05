07b35e7ab88e2c8706b913cffa29d3c8e137a97f
Kyungyun_host <kyungyun98@gmail.com>
2022-04-06 00:07:53 +0900

    usleep 100000

--- project1/main_process.c
+++ project1/main_process.c
@@ -64,7 +64,7 @@ int main_process(int shm_input_id, int shm_output_id)
 		}
 		prev_key = cur_key;
 		
-		usleep(10000);
+		usleep(100000);
 
 		//모드에 맞게 입력을 처리하고 output process로 넘긴다
 		//output process에서는 디바이스 파일 열어서 디바이스 상태만 바꿔주면 됨
