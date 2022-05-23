02a5aadc3983cda15c6c474c990cc22f67f6b0de
movie <kyungyun98@naver.com>
2022-05-24 02:08:20 +0900

    3 sec

--- project3/module/stopwatch.h
+++ project3/module/stopwatch.h
@@ -9,7 +9,7 @@
 #define DEV_DRIVER_MAJOR 242
 // register physical address
 #define IOM_FND_ADDRESS 0x08000004
-// keys
+// buttons interrupt
 #define HOME_KEY IMX_GPIO_NR(1,11)
 #define BACK_KEY IMX_GPIO_NR(1,12)
 #define VOLUP_KEY IMX_GPIO_NR(2,15)
@@ -18,12 +18,15 @@
 #define SUCCESS 0
 
 /* enums */
+// device driver status
 enum {
 	DRIVER_NOT_USED, DRIVER_OPENED
 };
+// stopwatch status
 enum {
 	STOPWATCH_OFF, STOPWATCH_ON, STOPWATCH_PLAY, STOPWATCH_PAUSED
 };
+// terminate key status
 enum {
 	KEY_INIT, KEY_PRESSED
 };
